b55db5265d2e4832daf3b3ac7b0d95845bf2c3b2
flyleaf <flyleaf91@163.com>
2019-12-27 14:23:10 +0800

    迁移仓库

---
+++ 开发板配套例程/F407霸天虎开发板/2.HC05测试程序(不带液晶)/User/systick/bsp_SysTick.h
@@ -0,0 +1,15 @@
+#ifndef __SYSTICK_H
+#define __SYSTICK_H
+
+#include "stm32f4xx.h"
+
+#define TASK_ENABLE 0
+#define NumOfTask 3
+
+void SysTick_Init(void);
+void mdelay(unsigned long nTime);
+
+int get_tick_count(unsigned long *count);
+void TimeStamp_Increment(void);
+
+#endif /* __SYSTICK_H */
